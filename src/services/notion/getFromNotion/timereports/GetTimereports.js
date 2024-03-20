0c58c181fdf1be3ab314c343908d4cf3cd018229
Fredrik Halvarsson <Fredrik.Halvarsson@live.se>
2024-03-20 15:56:16 +0100

    Changed timereports to work with changes in server

--- src/services/notion/getFromNotion/timereports/GetTimereports.js
+++ src/services/notion/getFromNotion/timereports/GetTimereports.js
@@ -35,58 +35,66 @@ import {
 
 export default function GetTimereports () {
 
-    const [data, setData] = useState(null); 
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null); 
 
-    const fetchDataFromNotion = ()=>{
-        const payload = {
-        };
-        axios.post('http://localhost:3001/notion/api/get/timereports', payload)
-            .then(response => {
-                setData(response.data); 
-                console.log('Data hämtad från Notion:', response.data);
-            })
-            .catch(error => {  
-                console.error('Fel vid hämtning från Notion:', error);
-            });
-    };            
-    
-    useEffect(() => {
-    fetchDataFromNotion();
-    }, []);
-    
-    if(!data || !Array.isArray(data?.results)) {
-        return <p>Laddar data eller ingen data att visa...</p>
-    }
+  const fetchDataFromNotion = ()=>{
+      const payload = {
+      };
+      axios.get('http://localhost:3001/notion/api/get/timereports', payload)
+          .then(response => {
+              setData(response.data);
+              setLoading(false); 
+              console.log('Data hämtad från Notion:', response.data);
+          })
+          .catch(error => {
+              setError(error);
+              setLoading(false);
+              console.error('Fel vid hämtning från Notion:', error);
+          });
+  };            
+  
+  useEffect(() => {
+  fetchDataFromNotion();
+  }, []);
+  
+  if (loading) {
+    return <p>Laddar data...</p>;
+  }
+  if (error || !Array.isArray(data?.results)) {
+    return <p>Ett fel uppstod vid hämtning av data från Notion.</p>;
+  }
 
-    return (
-            
-               <div className='container' style={{marginTop: '20px', marginBottom: '100px', marginLeft: '10%'}}>
-            <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 400 }} aria-label="customized table">
-                <TableHead>
-                  <TableRow>
-                    <StyledTableCell>Person</StyledTableCell>
-                    <StyledTableCell >Project</StyledTableCell>
-                    <StyledTableCell >#Hours</StyledTableCell>
-                    <StyledTableCell >Date</StyledTableCell> 
-                    <StyledTableCell >Note</StyledTableCell> 
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {data.results.map((page, index) => {
-                  return (
-                    <StyledTableRow key={index}>
-                      <StyledTableCell>{page.properties.RollupName.rollup.array[0].title[0].plain_text ?? 'No Title'}</StyledTableCell>
-                      <StyledTableCell>{page.properties.RollupProject.rollup.array[0].title[0].plain_text ?? 'No Project'}</StyledTableCell>
-                      <StyledTableCell>{page.properties.Hours.number ?? 'No hours'}</StyledTableCell>
-                      <StyledTableCell>{page.properties.Date.date.start ?? 'No date'}</StyledTableCell>
-                      <StyledTableCell>{page.properties.Note.title[0].plain_text ?? 'No Note'}</StyledTableCell>
-                    </StyledTableRow>
-                    );
-                    })}
-                </TableBody>
-              </Table>
-            </TableContainer>          
-        </div>
-    );
+  return (
+          
+              <div className='container' style={{marginTop: '20px', marginBottom: '100px', marginLeft: '10%'}}>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 400 }} aria-label="customized table">
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell>Person</StyledTableCell>
+                  <StyledTableCell >Project</StyledTableCell>
+                  <StyledTableCell >#Hours</StyledTableCell>
+                  <StyledTableCell >Date</StyledTableCell> 
+                  <StyledTableCell >Note</StyledTableCell> 
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data.results.map((page, index) => {
+                return (
+                  <StyledTableRow key={index}>
+                    <StyledTableCell>{page.properties.RollupName.rollup.array[0].title[0].plain_text ?? 'No Title'}</StyledTableCell>
+                    <StyledTableCell>{page.properties.RollupProject.rollup.array[0].title[0].plain_text ?? 'No Project'}</StyledTableCell>
+                    <StyledTableCell>{page.properties.Hours.number ?? 'No hours'}</StyledTableCell>
+                    <StyledTableCell>{page.properties.Date.date.start ?? 'No date'}</StyledTableCell>
+                    <StyledTableCell>{page.properties.Note.title[0].plain_text ?? 'No Note'}</StyledTableCell>
+                  </StyledTableRow>
+                  );
+                  })}
+              </TableBody>
+            </Table>
+          </TableContainer>          
+      </div>
+  );
 }
